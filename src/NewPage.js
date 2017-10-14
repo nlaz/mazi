ab9c9230de38c417b20e4e7375cdbd816e27c4f5
Vivek Singh <vivek.m.singh93@gmail.com>
2017-10-14 16:20:28 -0400

    Updated the new page with a price an time page

--- src/NewPage.js
+++ src/NewPage.js
@@ -7,8 +7,10 @@ export default function NewPage({ storageValue }) {
         <div className="pure-u-1-1">
           <form className="pure-form">
             <fieldset className="pure-group">
-              <input type="text" className="pure-input-1-2" placeholder="A title" />
-              <textarea className="pure-input-1-2" placeholder="Textareas work too" />
+              <input type="text" className="pure-input-1-2" placeholder="Your request" />
+              <textarea className="pure-input-1-2" placeholder="Detail on your task. (ex. I'd like detail on the best crypto companies in North America, by location)." />
+              <input type="text" className="pure-input-1-2" placeholder="Highest price (ex. 1 ETH)" />
+              <input type="text" className="pure-input-1-2" placeholder="When do you need this completed by?" />
             </fieldset>
 
             <button type="submit" className="pure-button pure-input-1-2 pure-button-primary">
